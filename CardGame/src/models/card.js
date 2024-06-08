ece0b3019863293ee47e4596974d19c3dd8d810e
Rahul Ravula <rravula@andrew.cmu.edu>
2024-06-07 22:05:01 -0700

    Created Basic Card and Player Models

---
+++ CardGame/src/models/card.js
@@ -0,0 +1,9 @@
+class Card {
+  constructor(id, suit, value) {
+    this.id = id;
+    this.suit = suit;
+    this.value = value;
+  }
+}
+
+export default Card;
---
+++ CardGame/src/models/player.js
@@ -0,0 +1,9 @@
+class Player {
+  constructor(id, name, hand) {
+    this.id = id;
+    this.name = name;
+    this.hand = hand;
+  }
+}
+
+export default Player;
