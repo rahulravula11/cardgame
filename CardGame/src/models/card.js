441e9e0361c2304f1cf4b7542ffc00251c0fa5d4
Rahul Ravula <rravula@andrew.cmu.edu>
2024-06-08 11:27:38 -0700

    Deleted more unecessary files

--- CardGame/src/models/card.js
+++
@@ -1,9 +0,0 @@
-class Card {
-  constructor(id, suit, value) {
-    this.id = id;
-    this.suit = suit;
-    this.value = value;
-  }
-}
-
-export default Card;
--- CardGame/src/models/player.js
+++
@@ -1,9 +0,0 @@
-class Player {
-  constructor(id, name, hand) {
-    this.id = id;
-    this.name = name;
-    this.hand = hand;
-  }
-}
-
-export default Player;
